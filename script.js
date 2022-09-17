dbdfd594e8b2de7d07412cba5d0f24f09f4b1624
Naomi Carrigan <nhcarrigan@gmail.com>
2022-09-16 20:36:22 -0700

    fix: strip quotes

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-import { readFile } from 'fs/promises';
-import { join } from 'path';
+import { readFile } from "fs/promises";
+import { join } from "path";
 import { exec } from "child_process";
 import { promisify } from "util";
 
 const execAsync = promisify(exec);
 
 (async () => {
-    console.log("Starting!");
-    const script = await readFile(join(process.cwd(), "script.txt"), "utf8");
-    const lines = script.split("\n");
-    const reversedForCommitHistory = lines.reverse();
+  console.log("Starting!");
+  const script = await readFile(join(process.cwd(), "script.txt"), "utf8");
+  const lines = script.split("\n");
+  const reversedForCommitHistory = lines.reverse();
 
-    for (const line of reversedForCommitHistory) {
-        await execAsync(`git commit --allow-empty -m "${line}"`);
-    }
-    console.log("Push those changes!!");
+  for (const line of reversedForCommitHistory) {
+    await execAsync(`git commit --allow-empty -m "${line.replace(/"/g, "'")}"`);
+  }
+  console.log("Push those changes!!");
 })();
